5c4dbf7e650ebbc30a3880350acd13e479b8fd57
Lynn Barnett <lynnbarnett@Lynns-MacBook-Air.local>
2014-11-24 21:39:58 -0600

    Layout Fix

--- application/views/compose.php
+++ application/views/compose.php
@@ -3,18 +3,18 @@
 <form action="send" method="POST" role="form" class="form-horizontal">
 
     
-// Register Editor component to your page   
-
-<html>   
-<body>   
+// Register Editor component to your page 
+    <div class="col-lg-11">  
         <div class="form-group">
             <label for="to">To:</label>
             <input type="text" name="to" class="form-control">
         </div>
+
         <div class="form-group">
             <label for="subject">Subject:</label>
             <input type="text" name="subject" class="form-control">
         </div>
+
         <form id="form1" method="POST">   
             <?php   
                 // Create Editor instance and use Text property to load content into the RTE.  
@@ -26,17 +26,15 @@
                 // Render Editor 
                 echo $rte->GetString();  
             ?>   
-        </form>   
+        </form> 
+
     <div class="col-lg-1 col-lg-offset-7">
         <div class="form-group">
             <input type="submit" value="Send To All" class="btn btn-success btn-lg btn-block">
         </div>
     </div>
-</body>   
-</html> 
+</div>
+
 
         
-
-    
-</form>
 
