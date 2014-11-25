73312ec3d6580df37d0ef79eb7e2e046d1704979
Victoria Potvin <vdpotvin@gmail.com>
2014-11-24 19:46:59 -0600

    added subject field

--- application/views/compose.php
+++ application/views/compose.php
@@ -1,15 +1,19 @@
-<form action="send_email/send" method="POST" role="form" class="form-horizontal">
+<form action="send" method="POST" role="form" class="form-horizontal">
 
     <div class='col-lg-5 col-lg-offset-3'>
         <div class="form-group">
             <label for="to">To:</label>
             <input type="text" name="to" class="form-control">
         </div>
+        <div class="form-group">
+            <label for="subject">Subject:</label>
+            <input type="text" name="subject" class="form-control">
+        </div>
 
         <div class="form-group">
             <label for="message">Message:</label>
-            //this is obviously the wrong class
-            <input type="text_area" name="message" class="form-control" rows="15">
+
+            <input type="text_area" name="message" class="form-control" >
         </div>
     </div>
 
