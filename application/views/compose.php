d3ab94b2bb912fa506e11aff1a70f65a131edfe4
Lynn Barnett <lynnbarnett@Lynns-MacBook-Air.local>
2014-11-24 21:46:04 -0600

    layout try 2

--- application/views/compose.php
+++ application/views/compose.php
@@ -1,38 +1,36 @@
 <?php require_once "application/richtexteditor/include_rte.php" ?> 
+<div class="col-lg-11"> 
+    <form action="send" method="POST" role="form" class="form-horizontal">
+ 
+                <div class="form-group">
+                    <label for="to">To:</label>
+                    <input type="text" name="to" class="form-control">
+                </div>
 
-<form action="send" method="POST" role="form" class="form-horizontal">
+                <div class="form-group">
+                    <label for="subject">Subject:</label>
+                    <input type="text" name="subject" class="form-control">
+                </div>
 
-    
-// Register Editor component to your page 
-    <div class="col-lg-11">  
-        <div class="form-group">
-            <label for="to">To:</label>
-            <input type="text" name="to" class="form-control">
-        </div>
+                <!-- <form id="form1" method="POST">  -->  
+                    <?php   
+                        // Create Editor instance and use Text property to load content into the RTE.  
+                        $rte=new RichTextEditor();   
+                        $rte->Text="Type here"; 
+                        // Set a unique ID to Editor   
+                        $rte->ID="Editor1";    
+                        $rte->MvcInit();   
+                        // Render Editor 
+                        echo $rte->GetString();  
+                    ?>   
+                <!-- </form>  -->
 
-        <div class="form-group">
-            <label for="subject">Subject:</label>
-            <input type="text" name="subject" class="form-control">
-        </div>
-
-        <form id="form1" method="POST">   
-            <?php   
-                // Create Editor instance and use Text property to load content into the RTE.  
-                $rte=new RichTextEditor();   
-                $rte->Text="Type here"; 
-                // Set a unique ID to Editor   
-                $rte->ID="Editor1";    
-                $rte->MvcInit();   
-                // Render Editor 
-                echo $rte->GetString();  
-            ?>   
-        </form> 
-
-    <div class="col-lg-1 col-lg-offset-7">
-        <div class="form-group">
-            <input type="submit" value="Send To All" class="btn btn-success btn-lg btn-block">
-        </div>
-    </div>
+                <div class="col-lg-1 col-lg-offset-7">
+                    <div class="form-group">
+                        <input type="submit" value="Send To All" class="btn btn-success btn-lg btn-block">
+                    </div>
+                </div>
+    </form>
 </div>
 
 
